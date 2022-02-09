ce713a187417d42d8ba0a7b082d26751ea4830a1
Joshua T J <joshuajose312@gmail.com>
2022-02-09 21:36:39 +0530

    contactform and ui update

--- script.js
+++ script.js
@@ -114,3 +114,19 @@ $('.more').click(
   }
 )
 });
+
+
+// scroll to top button
+//Get the button
+var mybutton = document.getElementById("goto-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
